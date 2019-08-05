fa7bde04c4a0139f1bd119ac8b52d05c1a97d4c5
yuuto <yuuttoo@gmail.com>
2019-08-05 23:32:33 +0800

    串接API完成

---
+++ index.js
@@ -0,0 +1,64 @@
+//jshint esversion: 6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request= require('request');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", (req, res) => {
+    //console.log(req.body.crypto);
+
+
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+
+    var amount = req.body.amount;
+
+
+    var options = {
+        url: "https://apiv2.bitcoinaverage.com/convert/global",
+        
+        method: "GET", 
+        qs: {
+            from: crypto,
+            to: fiat,
+            amount: amount,
+        }
+    };
+
+    request(options, (error, response, body) => {
+
+    var data = JSON.parse(body);
+    var price = data.price;
+
+    console.log(price);
+
+    var currentTime = data.time;
+    
+
+    res.write("<p>The current date is " + currentTime + "</p>");
+   
+
+    res.write("<h1>" + amount + " of " + crypto + " is currently worth " + price + fiat + "</h1>");
+
+    res.send();
+
+});
+
+
+
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Serveris running on port 3000.');
+})
